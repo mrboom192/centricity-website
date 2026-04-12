00c933d4d1f1f20dedf7a703986fef096d7390a1
Jason Nguyen <mrboom192@gmail.com>
2026-04-12 18:36:26 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,58 @@
+import CardNav from "@/components/card-nav";
+import Iridescence from "@/components/iridescence";
+import logo from "@/../public/logo.svg";
+
+export default function Home() {
+  const items = [
+    {
+      label: "About",
+      bgColor: "#0D0716",
+      textColor: "#fff",
+      links: [
+        { label: "Company", ariaLabel: "About Company" },
+        { label: "Careers", ariaLabel: "About Careers" },
+      ],
+    },
+    {
+      label: "Projects",
+      bgColor: "#170D27",
+      textColor: "#fff",
+      links: [
+        { label: "Featured", ariaLabel: "Featured Projects" },
+        { label: "Case Studies", ariaLabel: "Project Case Studies" },
+      ],
+    },
+    {
+      label: "Contact",
+      bgColor: "#271E37",
+      textColor: "#fff",
+      links: [
+        { label: "Email", ariaLabel: "Email us" },
+        { label: "Twitter", ariaLabel: "Twitter" },
+        { label: "LinkedIn", ariaLabel: "LinkedIn" },
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <Iridescence
+        color={[0.5, 0.6, 0.8]}
+        mouseReact
+        amplitude={0.1}
+        speed={1}
+      />
+      <CardNav
+        logo={logo}
+        logoAlt="Company Logo"
+        items={items}
+        baseColor="#fff"
+        menuColor="#000"
+        buttonBgColor="#111"
+        buttonTextColor="#fff"
+        ease="power3.out"
+        theme="light"
+      />
+    </>
+  );
+}
